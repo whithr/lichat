6dcfc3d167ad152c4fd14a4acc198c785cc69d27
Haley Whitman <97698963+whithr@users.noreply.github.com>
2023-06-25 23:50:02 -0700

    🔧 erase prisma/drizzle

--- src/env.mjs
+++ src/env.mjs
@@ -7,8 +7,9 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
+    DATABASE_URL: z.string().url(),
+    DIRECT_URL: z.string().url(),
   },
 
   /**
@@ -25,8 +26,9 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
